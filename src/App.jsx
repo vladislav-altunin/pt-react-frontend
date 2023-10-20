372e9f1c4d0bee44d8fc5b19ecb4a8f7cf93a723
Vladislav Altunin <vladislav.altunin@gmail.com>
2023-10-20 22:52:04 +0300

    edit: h1 to test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vlad + React</h1>
+      <h1>Vlad + Mahdi</h1>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
--- vite.config.js
+++ vite.config.js
@@ -49,5 +49,51 @@ permissions and other settings on github:
 
 - settings => actions => general / workflow permissions: read and write
 - settings => pages => build and deployment / source: deploy from branch / branch: gh-pages
----- source: github actions should work as well
+----npm run deploy // to deploy changes with this settings
+- settings => pages => build and deployment / source: github actions
+----requires a workwlow i.e. deploy.yml, them deployed automatically after push
+
+HERE IS AN EXAMPLE
+
+Create .gihub/workflows folders in the root dir of the app
+Create a file i.e. in this example node.js.yml but can be for example deploy.ym (if I'm not wrong)
+
+# This workflow will do a clean installation of node dependencies, cache/restore them, build the source code and run tests across different versions of node
+# For more information see: https://docs.github.com/en/actions/automating-builds-and-tests/building-and-testing-nodejs
+
+name: Node.js CI
+
+on:
+  push:
+    branches: ['main']
+  pull_request:
+    branches: ['main']
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [18.x]
+        # See supported Node.js release schedule at https://nodejs.org/en/about/releases/
+
+    steps:
+      - uses: actions/checkout@v3
+      - name: Use Node.js ${{ matrix.node-version }}
+        uses: actions/setup-node@v3
+        with:
+          node-version: ${{ matrix.node-version }}
+          cache: 'npm'
+      - run: npm ci
+      - run: npm run build --if-present
+      - run: npm test
+
+      - name: Deploy with gh-pages
+        run: |
+          git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+          npm run deploy -- -u "github-actions-bot <support+actions@github.com>"
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          
 */
