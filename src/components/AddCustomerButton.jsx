f75c12e27ec08ee81c0b4be6b8840749d7216c30
Vladislav Altunin <vladislav.altunin@gmail.com>
2023-11-05 00:23:02 +0200

    add: modal (add customer

--- src/components/AddCustomerButton.jsx
+++ src/components/AddCustomerButton.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import Button from '@mui/joy/Button';
 import { Box } from '@mui/material';
 import Add from '@mui/icons-material/Add';
+import AddCustomerModal from './AddCustomerModal';
 
 export default function AddCustomerButton() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <Box display="flex" flexDirection="column" alignItems="flex-end">
       <Button
         variant="outlined"
+        onClick={handleClickOpen}
         startDecorator={<Add />}
         style={{ margin: 15 }}
       >
         Add customer
       </Button>
+      <AddCustomerModal open={open} setOpen={setOpen} />
     </Box>
   );
 }
